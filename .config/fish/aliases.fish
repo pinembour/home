a2881e752b5984f093dd0286e6b18cdb6f578212
Pinembour <pinembour@teppelin.fr>
2020-02-24 23:27:48 +0000

    Added aliases.fish

---
+++ .config/fish/aliases.fish
@@ -0,0 +1,24 @@
+
+# Aliases
+alias ll="ls --color=auto -alh"
+alias ls="ls --color=auto -h"
+alias grep="grep --color=auto"
+alias less="less -r"
+alias unzip-all="7z x -y '*.zip'"
+
+# System aliases
+alias reboot="sudo reboot"
+
+# Git aliases
+alias st="git status"
+alias clone="git clone"
+
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias mkdir='mkdir -p'
+alias rm='rm --interactive=once'
+alias cp='cp --interactive'
+alias mv='mv --interactive'
+alias performance='sudo cpufreq-set -g performance'
+alias powersave='sudo cpufreq-set -g powersave'
+alias teppelin='ssh teppelin'
